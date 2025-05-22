ac4d9ed5b8c88c06a6b1e3f2fbb2102cb133ebcf
saiqo <saiqouu@gmail.com>
2025-05-22 14:32:20 +0200

    Remove app

--- application/README.md
+++
@@ -1,4 +0,0 @@
-# Application gestion cluster proxmox
-
-Liens utiles :
-- https://github.com/miguelgrinberg/flasky/blob/master/config.py
--- application/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:password@localhost/nom_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
